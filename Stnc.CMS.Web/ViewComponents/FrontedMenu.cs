80d5ac85b20faa1300bf4a0a1f2a42b324da49e6
stnc-lenova <selmantunc@yahoo.com>
2020-11-05 00:49:15 +0300

    feat: compnnt done

--- Stnc.CMS.Web/ViewComponents/FrontedMenu.cs
+++ Stnc.CMS.Web/ViewComponents/FrontedMenu.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Newtonsoft.Json.Linq;
 using Stnc.CMS.Business.Interfaces;
 using Stnc.CMS.DTO.DTOs.AppUserDtos;
@@ -46,57 +48,56 @@ namespace Stnc.CMS.Web.ViewComponents
         public IViewComponentResult Invoke()
         {
 
-            var json = _optionsService.GetOptionName("front-menu");
-
+            string  json = "{\"menuList\":" + _optionsService.GetOptionName("front-menu")+ "}";
             string menu = "";
-  
-            try
-            {
-                var jObject = JObject.Parse(json);
 
-                if (jObject != null)
-                {
-                    JArray dataArray = (JArray)jObject["menuList"];
+                      try
+                      {
+                          var jObject = JObject.Parse(json);
 
-                    if (dataArray != null)
-                    {
-                        foreach (var item in dataArray)
-                        {
+                          if (jObject != null)
+                          {
+                              JArray dataArray = (JArray)jObject["menuList"];
 
-                            if (item["children"] != null)
-                            {
-                                menu += "<li class=\"has-children\">\n";
-                                menu += htmlReturn(item["target"].ToString(), item["href"].ToString(), item["text"].ToString());
+                              if (dataArray != null)
+                              {
+                                  foreach (var item in dataArray)
+                                  {
 
-                                menu += "<ul class=\"dropdown\">\n";
-                                foreach (var itemChild in item["children"])
-                                {
-                                    menu += htmlReturn(itemChild["target"].ToString(), itemChild["href"].ToString(), itemChild["text"].ToString());
-                                }
-                                menu += "</ul>\n";
-                                menu += "</li>";
-                            }
-                            else
-                            {
-                                menu += "<li>\n";
-                                menu += htmlReturn(item["target"].ToString(), item["href"].ToString(), item["text"].ToString(), true);
-                                menu += "</li>";
-                            }
-                        }
+                                      if (item["children"] != null)
+                                      {
+                                          menu += "<li class=\"has-children\">\n";
+                                          menu += htmlReturn(item["target"].ToString(), item["href"].ToString(), item["text"].ToString(), true);
 
-                    }
-                }
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+                                          menu += "<ul class=\"dropdown\">\n";
+                                          foreach (var itemChild in item["children"])
+                                          {
+                                              menu += htmlReturn(itemChild["target"].ToString(), itemChild["href"].ToString(), itemChild["text"].ToString());
+                                          }
+                                          menu += "</ul>\n";
+                                          menu += "</li>\n";
+                                      }
+                                      else
+                                      {
+                                          menu += "<li>\n";
+                                          menu += htmlReturn(item["target"].ToString(), item["href"].ToString(), item["text"].ToString(), true);
+                                          menu += "</li>\n";
+                                      }
+                                  }
 
-
-            return View(menu);
+                              }
+                          }
+                      }
+                      catch (Exception)
+                      {
+                          throw;
+                      }
             
 
-          
+            return new HtmlContentViewComponentResult(new HtmlString(menu));
+
+
+
         }
     }
 }
