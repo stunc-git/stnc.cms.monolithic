18e99dee05e242b4a1725e8aea0a026f909c2118
selman_ofis <selmantunc@hotmail.com>
2020-12-07 16:03:22 +0300

    feat: icerik

--- Stnc.CMS.Business/Concrete/DosyaManager.cs
+++ Stnc.CMS.Business/Concrete/DosyaManager.cs
@@ -7,7 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-
+using Microsoft.AspNetCore.Hosting;
 namespace Stnc.CMS.Business.Concrete
 {
     public class DosyaManager : IDosyaService
@@ -81,6 +81,7 @@ namespace Stnc.CMS.Business.Concrete
             BaseFont baseFont = BaseFont.CreateFont(calibriTtf, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
 
             Font font = new Font(baseFont, 12, Font.NORMAL); //FontFactory.GetFont("Arial", 10, BaseColor.Black
+            Font fontBold = new Font(baseFont, 11, Font.BOLD); //FontFactory.GetFont("Arial", 10, BaseColor.Black
             Font fontMiniSmall = new Font(baseFont, 5, Font.NORMAL, BaseColor.Blue); //FontFactory.GetFont("Arial", 10, BaseColor.Black
 
             Document document = new Document(PageSize.Letter, 50f, 10f, 10f, 0f);
@@ -129,37 +130,61 @@ namespace Stnc.CMS.Business.Concrete
           
 
             PdfPTable table2 = new PdfPTable(2);
-            PdfPCell cell2 = new PdfPCell(new Phrase("ERCİYES ÜNİVERSİTESİ TIP FAKÜLTESİ", font));
+            PdfPCell cell2 = new PdfPCell(new Phrase("ERCİYES ÜNİVERSİTESİ TIP FAKÜLTESİ", fontBold));
             cell2.Border = Rectangle.NO_BORDER;
             cell2.HorizontalAlignment = PdfCell.ALIGN_LEFT;
+            cell2.PaddingTop = 10;
+            cell2.PaddingBottom = 10;
             table2.AddCell(cell2);
 
 
-            cell2 = new PdfPCell(new Phrase("11.12.2019", font));
+            cell2 = new PdfPCell(new Phrase("11.12.2019", fontBold));
             cell2.HorizontalAlignment = PdfCell.ALIGN_RIGHT;
             cell2.Border = Rectangle.NO_BORDER;
+            cell2.PaddingTop = 10;
+            cell2.PaddingBottom = 10;
             table2.AddCell(cell2);
 
-            var p = new Paragraph(16, "PLASTİK REKONSTRÜKTİF VE ESTETİK CERRAHİ ANABİLİM DALI BAŞKANLIĞI", font);
-            p.Alignment = Element.ALIGN_LEFT; // p.IndentationLeft = 100;
 
-            var p2 = new Paragraph(16, "Proforma Fatura", font);
+            PdfPTable table4 = new PdfPTable(1);
+            PdfPCell cell3= new PdfPCell(new Phrase("PLASTİK REKONSTRÜKTİF VE ESTETİK CERRAHİ ANABİLİM DALI BAŞKANLIĞI", fontBold));
+            cell3.HorizontalAlignment = PdfCell.ALIGN_LEFT;
+            cell3.Border = Rectangle.NO_BORDER;
+            cell2.PaddingTop = 10;
+            cell2.PaddingBottom = 10;
+            table4.AddCell(cell3);
+
+
+
+            var p2 = new Paragraph(16, "Proforma Fatura", fontBold);
+            cell2.PaddingTop = 10;
+            cell2.PaddingBottom = 10;
             p2.Alignment = Element.ALIGN_CENTER;
 
             /// ****** HEADER *******//// 
-            PdfPCell cell = new PdfPCell(new Phrase("Hayvan Türü -  Yaşı", font));
+            PdfPCell cell = new PdfPCell(new Phrase("Hayvan Türü -  Yaşı", fontBold));
+            cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
-            cell = new PdfPCell(new Phrase("Fiyat", font));
+            cell = new PdfPCell(new Phrase("Fiyat", fontBold));
+            cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
-            cell = new PdfPCell(new Phrase("İstenen Hayvan Sayısı", font));
+            cell = new PdfPCell(new Phrase("İstenen Hayvan Sayısı", fontBold));
+            cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
-            cell = new PdfPCell(new Phrase("Destek istenen Hayvan Sayısı", font));
+            cell = new PdfPCell(new Phrase("Destek istenen Hayvan Sayısı", fontBold));
+            cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
-            cell = new PdfPCell(new Phrase("Bakım Desteği Gün sayısı", font));
+            cell = new PdfPCell(new Phrase("Bakım Desteği Gün sayısı", fontBold));
+            cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
             /// ****** HEADER end*******//// 
 
@@ -167,14 +192,17 @@ namespace Stnc.CMS.Business.Concrete
             /// ****** içerik *******//// 
             cell = new PdfPCell(new Phrase(" Fare (Balb-C) / 8 Haftalık Yaşa Kadar ", font));
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
             cell = new PdfPCell(new Phrase("5 TL", font));
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
             cell = new PdfPCell(new Phrase("10", font));
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
 
             cell = new PdfPCell(new Phrase("5", font));
@@ -184,28 +212,14 @@ namespace Stnc.CMS.Business.Concrete
 
             cell = new PdfPCell(new Phrase("10", font));
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            cell.VerticalAlignment = Element.ALIGN_MIDDLE;
             table.AddCell(cell);
             /// ****** HEADER end*******//// 
 
-            /*
-            cell = new PdfPCell(new Phrase("agirlik:420 gr / Cinsiyet: Erkek / Ötenazi[1 TL]: Evet  ", font));
-            cell.Colspan = 5;
-            cell.Border = Rectangle.NO_BORDER;
-            cell.PaddingTop = -7;
-            table.AddCell(cell);
-            */
+   
 
 
-            
 
-
-                   string text = @"
-                    *Fiyatlar, Erciyes Üniversitesi’ne mensup araştırmacılar için ve kurum dışı taleplerde fiyatlar geçerlidir.
-                    **Diyabet projeleri kapsamında takip edilecek hayvanlar için % 50 daha fazla günlük bakım ücreti alınır.
-                    ***Ötenazi ve özel cerrahi operasyonlar hariç; post operatif bakım, anestezi, enjeksiyon, kan alma, hayvan tutma, hayvan tartma ve gavaj uygulaması gibi desteklerden oluşmaktadır.
-                     ****Ötenazi için gerekli kimyasal maddeler araştırmacı tarafından temin edilir. ";
-            var p3 = new Paragraph(12, text, fontMiniSmall);
-            p3.IndentationRight = 50f;
 
             cell = new PdfPCell(new Phrase("Ağırlık: 420 gr | Cinsiyet: Erkek | Ötenazi [1 TL]: Evet", font));
             cell.Colspan = 5;
@@ -214,7 +228,7 @@ namespace Stnc.CMS.Business.Concrete
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
             table.AddCell(cell);
 
-            cell = new PdfPCell(new Phrase(" Destek Talep Türü Seçenekleri   ",font));
+            cell = new PdfPCell(new Phrase(" Destek Talep Türü Seçenekleri   ", fontBold));
             cell.Colspan = 5;
             cell.PaddingTop = 5;
             cell.PaddingBottom = 5;
@@ -222,7 +236,7 @@ namespace Stnc.CMS.Business.Concrete
             table.AddCell(cell);
 
 
-            cell = new PdfPCell(new Phrase("MADDE UYGULAMA (ENJEKSİYON, GAVAJ v.s.) ÜCRETİ / Fiyat 10 ₺   ", font));
+            cell = new PdfPCell(new Phrase("MADDE UYGULAMA (ENJEKSİYON, GAVAJ v.s.)  / Fiyat 10 ₺   ", font));
             cell.Colspan = 5;
             cell.PaddingTop = 5;
             cell.PaddingBottom = 5;
@@ -231,7 +245,7 @@ namespace Stnc.CMS.Business.Concrete
 
 
 
-            cell = new PdfPCell(new Phrase("KAN ALMA (İNTRAKARDİYOK, İNTRAVENÜZ v.s.) ÜCRETİ / Fiyat 5 ₺  ", font));
+            cell = new PdfPCell(new Phrase("KAN ALMA (İNTRAKARDİYOK, İNTRAVENÜZ v.s.)  / Fiyat 5 ₺  ", font));
             cell.Colspan = 5;
             cell.PaddingTop = 5;
             cell.PaddingBottom = 5;
@@ -239,7 +253,7 @@ namespace Stnc.CMS.Business.Concrete
             table.AddCell(cell);
 
 
-            cell = new PdfPCell(new Phrase("Toplam  ", font));
+            cell = new PdfPCell(new Phrase("Birim Toplamı  ", font));
             cell.Colspan = 4;
             cell.PaddingTop = 5;
             cell.PaddingBottom = 5;
@@ -247,33 +261,76 @@ namespace Stnc.CMS.Business.Concrete
             table.AddCell(cell);
 
 
-            cell = new PdfPCell(new Phrase("5", font));
+            cell = new PdfPCell(new Phrase("5 ₺", font));
             cell.Colspan = 1;
             cell.PaddingTop = 5;
             cell.PaddingBottom = 5;
             cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
             table.AddCell(cell);
 
+            PdfPTable table5 = new PdfPTable(2);
+            PdfPCell cell4 = new PdfPCell(new Phrase("Genel Toplam", fontBold));
+           // cell4.Colspan = 2;
+            cell4.PaddingTop = 5;
+            cell4.PaddingBottom = 5;
+            cell4.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            table5.AddCell(cell4);
+
+
+            cell4 = new PdfPCell(new Phrase("5 ₺", font));
+          //  cell4.Colspan = 1;
+            cell4.PaddingTop = 5;
+            cell4.PaddingBottom = 5;
+            cell4.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+            table5.AddCell(cell4);
+
+
+            string text = @"
+                    *Fiyatlar, Erciyes Üniversitesi’ne mensup araştırmacılar için ve kurum dışı taleplerde fiyatlar geçerlidir.
+                    **Diyabet projeleri kapsamında takip edilecek hayvanlar için % 50 daha fazla günlük bakım ücreti alınır.
+                    ***Ötenazi ve özel cerrahi operasyonlar hariç; post operatif bakım, anestezi, enjeksiyon, kan alma, hayvan tutma, hayvan tartma ve gavaj uygulaması gibi desteklerden oluşmaktadır.
+                     ****Ötenazi için gerekli kimyasal maddeler araştırmacı tarafından temin edilir. ";
+      
+
+            PdfPTable table6 = new PdfPTable(1);
+            PdfPCell cell5 = new PdfPCell(new Paragraph(12, text, fontMiniSmall));
+            cell5.PaddingTop = 5;
+            cell5.PaddingBottom = 5;
+            cell5.Border = Rectangle.NO_BORDER;
+            cell5.HorizontalAlignment = PdfCell.ALIGN_LEFT;
+            table6.AddCell(cell5);
+
+            Image imzaImg = Image.GetInstance("D:\\imza.png");
+
+            //var scalePercentImza = (((document.PageSize.Width / imzaImg.Width) * 80) - 5);
+            //imzaImg.ScalePercent(scalePercentImza);
+
+
+
+            PdfPTable table6Img = new PdfPTable(1);
+            PdfPCell imageCell = new PdfPCell(imzaImg);
+            imageCell.Border = Rectangle.NO_BORDER;
+            imageCell.HorizontalAlignment = PdfCell.ALIGN_LEFT;
 
             // metadata
-            document.AddCreator("EstimatorX.com");
-            document.AddKeywords("estimation");
-            document.AddAuthor("ww");
-            document.AddSubject("ee");
-            document.AddTitle("eee");
-
-
+            document.AddCreator("erciyes.edu.tr");
+            document.AddKeywords("Erciyes üniversitesi Bilgi İşlem Daire Başkanlığı");
+            document.AddAuthor("@stnc");
+            document.AddSubject("Proforma Fatura Oluşturucu");
+            document.AddTitle("Proforma Fatura");
           
             document.Add(image); //logo 
    
             document.Add(table2); //bolum ve saat yazan 
-            document.Add(p);
+            document.Add(table4);
             document.Add(p2);
 
 
             document.Add(table);
+            document.Add(table5);
+            document.Add(table6);
+            document.Add(table6Img);
 
-            document.Add(p3);
             document.Close();
 
             return returnPath;
